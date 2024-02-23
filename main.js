c8c86e2633ad3b283108af309b600a96bb865d78
JavlonKarimbaev <jkslz.kslz@gmail.com>
2024-02-23 11:28:02 +0500

    java script

---
+++ main.js
@@ -0,0 +1,30 @@
+console.error("hatolik")
+console.info("bufet")
+console.warn("ummm")
+console.log("salom")
+console.clear()
+
+
+let ism = prompt('ismingizni kiriting');
+
+console.log('ism: ' +ism)
+
+let familiya = prompt('Familiyangizni kiriting');
+
+console.log('familiya: ' +familiya)
+
+let yosh = prompt('yoshingizni kiriting');
+
+console.log('yosh: ' +yosh)
+
+let fan = prompt('Yoqtirgan faningizni yozing');
+
+console.log('fan: ' +fan)
+
+let tuman = prompt('Yashaydigan tumaningiznni kiriting');
+
+console.log('tuman: ' +tuman)
+
+let = alert('malumotlaringizni kiritkaningiz uchun tashakkur')
+
+
